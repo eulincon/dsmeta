689d47b8c20625caebd249c1966e946b57d2efe3
Lincon Brito <linconsilva08@gmail.com>
2022-07-12 23:33:33 -0300

    project created

---
+++ backend/src/main/kotlin/com/devsuperior/dsmeta/DsmetaApplication.kt
@@ -0,0 +1,11 @@
+package com.devsuperior.dsmeta
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DsmetaApplication
+
+fun main(args: Array<String>) {
+	runApplication<DsmetaApplication>(*args)
+}
